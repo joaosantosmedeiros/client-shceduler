dd3e36ba4f1ad884babf6f68b0b6277753bc37df
João Pedro <jopesame@gmail.com>
2026-06-06 18:30:56 -0300

    feat: add arquivos de constantes

---
+++ src/main/java/god/joaopedro/client_scheduler/utils/Constants.java
@@ -0,0 +1,8 @@
+package god.joaopedro.client_scheduler.utils;
+
+public class Constants {
+    public static final String CPF = "cpf";
+    
+    public static final String INVALID_MESSAGE = "must be valid";
+    public static final String IN_USE = "is already in use";
+}
