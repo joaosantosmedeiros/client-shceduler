00f5b47f1f9a6004d812da062508f5c03add40e2
João Pedro <jopesame@gmail.com>
2026-06-06 18:33:17 -0300

    fix: correção no método de criar um novo paciente

--- src/main/java/god/joaopedro/client_scheduler/commons/patient/service/PatientService.java
+++ src/main/java/god/joaopedro/client_scheduler/commons/patient/service/PatientService.java
@@ -3,6 +3,9 @@ package god.joaopedro.client_scheduler.commons.patient.service;
 import god.joaopedro.client_scheduler.commons.patient.model.Patient;
 import god.joaopedro.client_scheduler.commons.patient.model.dto.PatientDTO;
 import god.joaopedro.client_scheduler.commons.patient.repository.PatientRepository;
+import god.joaopedro.client_scheduler.exceptions.InvalidFieldException;
+import god.joaopedro.client_scheduler.utils.Constants;
+import god.joaopedro.client_scheduler.utils.CpfValidator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,9 +24,11 @@ public class PatientService {
             throw new IllegalArgumentException("Patient must not be null.");
         }
 
-        if(patientDTO.cpf() != null && repository.findByCpf(patientDTO.cpf()).isPresent()) {
-            throw new IllegalArgumentException("CPF is already in use.");
-        }
+        if(!CpfValidator.validate(patientDTO.cpf()))
+            throw new InvalidFieldException(Constants.CPF, Constants.INVALID_MESSAGE);
+
+        if(repository.findByCpf(patientDTO.cpf()).isPresent())
+            throw new InvalidFieldException(Constants.CPF, Constants.IN_USE);
 
         return repository.save(new Patient(patientDTO));
     }
