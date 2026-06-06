53965c8a8e37d4bf5d16c994f83fbf2dab23ce37
João Pedro <jopesame@gmail.com>
2026-06-06 18:30:30 -0300

    fix: add timezone do brasil

--- src/main/java/god/joaopedro/client_scheduler/ClientSchedulerApplication.java
+++ src/main/java/god/joaopedro/client_scheduler/ClientSchedulerApplication.java
@@ -3,10 +3,13 @@ package god.joaopedro.client_scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ClientSchedulerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT-03:00"));
 		SpringApplication.run(ClientSchedulerApplication.class, args);
 	}
 
